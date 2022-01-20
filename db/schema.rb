135d5c2c990c63a0bb5dd4ce38d6bb8475177f40
Vinicius Campanholi <viniciuscampanholi@hotmail.com>
2022-01-20 17:49:19 -0300

    Correção do metodo destroy

--- db/schema.rb
+++ db/schema.rb
@@ -20,15 +20,6 @@ ActiveRecord::Schema.define(version: 2021_12_17_190430) do
     t.string "status"
   end
 
-  create_table "commenters", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
-    t.integer "article_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["article_id"], name: "index_commenters_on_article_id"
-  end
-
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
@@ -39,6 +30,5 @@ ActiveRecord::Schema.define(version: 2021_12_17_190430) do
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
-  add_foreign_key "commenters", "articles"
   add_foreign_key "comments", "articles"
 end
